de6974c2b4fcd2b0389077d598e966feef18b256
David Eilers <deilers@osd.wednet.edu>
2022-04-27 08:25:04 -0700

    auto output option

--- main.go
+++ main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 var voice = flag.String("v", "en_us_002", "Use a good voice like "+strings.Join(voices, " or "))
 var output = flag.String("o", "", "Output to an mp3 file")
+var autoOutput = flag.Bool("O", false, "Output to a file named text_voice.mp3")
 var quiet = flag.Bool("q", false, "Don't play sound audibly")
 
 func main() {
@@ -19,6 +22,9 @@ func main() {
 	text := "This is a test"
 	if len(flag.Args()) > 0 {
 		text = strings.Join(flag.Args(), " ")
+	}
+	if *autoOutput {
+		*output = filepath.Join(".", fmt.Sprintf("%s_%s.mp3", text, *voice))
 	}
 	mp3Data, err := tiktoktts(text, *voice)
 	if err != nil {
