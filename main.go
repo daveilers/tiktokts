c62135a98f8e956c0505fcdbd5cd75c69a38d38a
David Eilers <deilers@osd.wednet.edu>
2022-04-26 19:32:10 -0700

    add output and quiet option

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"flag"
 	"io"
 	"log"
+	"os"
 	"strings"
 	"time"
 )
 
 var voice = flag.String("v", "en_us_002", "Use a good voice like "+strings.Join(voices, " or "))
+var output = flag.String("o", "", "Output to an mp3 file")
+var quiet = flag.Bool("q", false, "Don't play sound audibly")
 
 func main() {
 	flag.Parse()
@@ -17,15 +20,27 @@ func main() {
 	if len(flag.Args()) > 0 {
 		text = strings.Join(flag.Args(), " ")
 	}
-	sayText(text)
-	time.Sleep(time.Second)
+	mp3Data, err := tiktoktts(text, *voice)
+	if err != nil {
+		log.Printf("Ack %q %v: %v", text, *voice, err)
+	}
+	if !*quiet {
+		playMp3(io.NopCloser(bytes.NewReader(mp3Data)))
+		time.Sleep(time.Second)
+	}
+	if *output != "" {
+		err = os.WriteFile(*output, mp3Data, 0700)
+		if err != nil {
+			log.Printf("Problem writing file %q: %v", *output, err)
+		}
+	}
 }
 
 func sayText(s string) {
-	data, err := tiktoktts(s, *voice)
+	mp3Data, err := tiktoktts(s, *voice)
 	if err != nil {
 		log.Printf("Ack %q %v: %v", s, *voice, err)
 		return
 	}
-	playMp3(io.NopCloser(bytes.NewReader(data)))
+	playMp3(io.NopCloser(bytes.NewReader(mp3Data)))
 }
